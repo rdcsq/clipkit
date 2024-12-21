52f50b8620430e60fe59f8529433504c1d448a3f
rdcsq <git@rdcsq.com>
2024-12-20 22:52:21 -0700

    error handling

--- app/.server/accounts.ts
+++ app/.server/accounts.ts
@@ -9,6 +9,9 @@ export class AccountsService {
         update users set username = ${newUsername} where id = ${userId}
       `;
     } catch (e) {
+      if (e instanceof this.sql.PostgresError && e.code == "23505") {
+        throw new Error("A user with that username already exists.");
+      }
       throw new Error("An error occured while updating the username", {
         cause: e,
       });
@@ -16,13 +19,13 @@ export class AccountsService {
   }
 
   async getUserDetails(userId: number) {
-    const [user] : [UserDetails?] = await this.sql`
+    const [user]: [UserDetails?] = await this.sql`
         select username from users where id = ${userId}
-    `
+    `;
     return user;
   }
 }
 
 type UserDetails = {
-    username: string;
-}
+  username: string;
+};
